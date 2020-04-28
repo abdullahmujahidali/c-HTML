e02122612907bb9d0a3a8fc4c3e292aa3fba1453
Abdullah Mujahid <48271080+abdullahmujahidali@users.noreply.github.com>
2020-04-28 23:25:54 +0500

    Update main.c
    removed useless data

--- main.c
+++ main.c
@@ -3,12 +3,12 @@
 #include<stdlib.h>
 int main()
 {
-   FILE *out;///Object to Creat Html Fule
-   out = fopen("output.html","w");//we Creat and Open an HTML file Names as: output.html->this file will be located after programe execution were you saved the programe
-   char name[25];//size of name you can increase or decrease name size...
-   char intro[1000];//Describe Yourself->you can incresae the words size from 1000 to another amount or also decrease it here...
-  
-   system("color b");//Background Color...
+   FILE *out; //file object for html type file
+   out = fopen("output.html","w");// create an html file 
+   char name[25];
+   char intro[1000];
+
+   system("color 7");//Background Color...
   
    printf("\n\t\t\t-----------------------------");
   
@@ -18,10 +18,10 @@ int main()
    gets(intro);
   
    fprintf(out, "<!DOCTYPE html>\n");
-   fprintf(out, "<head><title>%s</title></head>\n",name);//Set Name as Title in HTML Output File
-   fprintf(out, "<html><body background=\"background.jpg\" style=\"color:Black;font-family:georgia\">\n");//were background.jpg you can add your own image to set background image
-   fprintf(out, "<h1> &emsp;&emsp; %s</h3><h3> &emsp;&emsp; %s</h1>\n",name,intro); //just put your image name and extension...
-   //text color is black you can change it as Blue , Red etc as you want....
+   fprintf(out, "<head><title>%s</title></head>\n",name);
+   fprintf(out, "<html><body background=\"background.jpg\" style=\"color:Black;font-family:georgia\">\n");
+
+   fprintf(out, "<h1> %s</h3><h3>  %s</h1>\n",name,intro); 
 
    fprintf(out, "</body></html>");
    }
